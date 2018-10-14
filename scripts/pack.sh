e93ff64f21152a15d4310ad85b71d0c97259cffc
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-10-14 17:38:39 +0700

    Change pack command

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -1,5 +1,6 @@
 #!/bin/bash 
 
 docker build -t devinpeace/$IMAGE_NAME:$TAG .
+docker tag $IMAGE_NAME devinpeace/$IMAGE_NAME:$TAG
 docker login -u $DOCKER_LOGIN -p $DOCKER_PWD
-docker push devinpeace/$IMAGE_NAME:$TAG
+docker push devinpeace/$IMAGE_NAME
