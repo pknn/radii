8f8e134a8badda950985ecf307deabc1aefe54f8
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-10-14 12:22:00 +0700

    Use meinheld in production

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-scp -o "StrictHostKeyChecking no" docker-compose.yml pakanon@35.240.234.173:~/
-ssh -o "StrictHostKeyChecking no" pakanon@35.240.234.173 "sudo docker-compose pull && sudo docker-compose up -d"
+scp -o "StrictHostKeyChecking no" docker-compose.yml pakanon@35.193.135.74:~/
+ssh -o "StrictHostKeyChecking no" pakanon@35.193.135.74 "sudo docker-compose pull && sudo docker-compose up -d"
