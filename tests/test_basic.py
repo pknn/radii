26ce724dbdab7fa3666d2495d7d4bad119f49234
Pakanon Pantisawat <pakanon@Pakanons-MacBook-Pro.local>
2018-10-17 14:31:31 +0700

    Edit configuration

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,4 @@
 import pytest
-
 from app.models import User
 from app import db
 
@@ -7,5 +6,9 @@ from app import db
 def test_simple():
     user = User(username="Pakanon")
     db.session.add(user)
+    db.session.commit()
+
+    user = User.query.all()[0]
+    db.session.delete(user)
     db.session.commit()
     assert 2 + 3 == 5
