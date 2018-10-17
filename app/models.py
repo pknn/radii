b8eb4e5e44b24a0c3666fcf05ea8439fa0c9cf16
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-10-17 15:54:41 +0700

    Finished model/Events

--- app/models.py
+++ app/models.py
@@ -1,1 +1,33 @@
+from datetime import datetime, timedelta
 from app import db
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True)
+    description = db.Column(db.String(250))
+    location = db.Column(db.String(100), index=True)
+    image_url = db.Column(db.String(200), index=True)
+    date_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def is_event_nearby(self):
+        pass
+        # return Event.location
+
+    def is_event_passed(self):
+        # return true if passed and false if not
+        present = datetime.utcnow()
+        event = self.date_time
+        if event < present:
+            return True
+        else:
+            return False
+
+
+    def is_event_upcoming(self):
+        # before 3 days
+        event = self.date_time
+        upcoming = datetime.utcnow() + timedelta(days=3)
+        if upcoming < event:
+            return True
+        else:
+            return False
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,22 @@
 import pytest
-
+import datetime
+from app import models
+from app.models import Event
+from app import db
 
 def test_simple():
     assert 2 + 3 == 5
+
+def test_event_passed():
+    u = Event(date_time=datetime.datetime(2018, 12, 1, 10, 30 ,11))
+    db.session.add(u)
+    db.session.commit()
+
+    assert not u.is_event_passed()
+
+def test_event_upcoming():
+    u = Event(date_time=datetime.datetime(2018, 9, 1, 10, 30 ,11))
+    db.session.add(u)
+    db.session.commit()
+
+    assert not u.is_event_upcoming()
