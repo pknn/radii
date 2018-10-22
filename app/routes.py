fd8ee8c79ad020d709cc088596a053a9312ca8d0
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-10-23 02:59:52 +0700

    Index title changed

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,7 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html', title='Coming soon')
+    return render_template('index.html')
 
 
 @app.route('/event')
