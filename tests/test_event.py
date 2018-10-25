502965a4d1bc1561fcf6780d2eec4eef8fe5b9a7
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-10-25 15:50:58 +0700

    Added test_user

--- tests/test_event.py
+++ tests/test_event.py
@@ -18,13 +18,3 @@ def test_event_upcoming():
 
     assert not u.is_event_upcoming()
 
-def test_event_password():
-    u = User(username='poom', email='poom@example.com')
-    u.set_password('mypassword')
-
-    assert not u.check_password('anotherpassword')
-    assert u.check_password('mypassword')
-
-def test_event_username():
-    u = User(username='poom', email='poom@example.com')
-    assert u
---
+++ tests/test_user.py
@@ -0,0 +1,13 @@
+from app.models import Event,User
+from app import db
+
+def test_event_password():
+    u = User(username='poom', email='poom@example.com')
+    u.set_password('mypassword')
+
+    assert not u.check_password('anotherpassword')
+    assert u.check_password('mypassword')
+
+def test_event_username():
+    u = User(username='poom', email='poom@example.com')
+    assert u
