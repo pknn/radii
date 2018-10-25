1b54f7ccc050753af1330dcb2fa3a8a509f83457
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-10-25 14:36:06 +0700

    Added forms.py for LoginForm

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,12 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from sqlalchemy_utils import create_database, database_exists
+from app import routes, models
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
 if not database_exists(Config.SQLALCHEMY_DATABASE_URI):
@@ -15,5 +18,6 @@ if not database_exists(Config.SQLALCHEMY_DATABASE_URI):
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+login = LoginManager(app)
 
-from app import routes, models
+
