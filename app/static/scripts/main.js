243d278400f08954f3f68ca8878025e3527ee2ca
benzsupaluk <supaluk.j@ku.th>
2018-10-27 13:40:18 +0700

    Add toggle password visibility function

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -2,3 +2,13 @@ $('document').ready(function () {
     console.log('Hi');
 })
 
+$("#toggleButton").on("click", function() {
+    if ($("#password-input").attr('type') === 'password'){
+        $("#password-input").attr('type', 'text');
+        $("#toggleButton").html('<i class="fas fa-eye"></i>');
+
+    }else {
+        $("#password-input").attr('type', 'password');
+        $("#toggleButton").html('<i class="fas fa-eye-slash"></i>');
+    }
+});
