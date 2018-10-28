9b782169e09b56a286ebd1c8f116cbce9b77cc62
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-10-29 00:23:00 +0700

    Refactoring styles and add toggle button script

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,14 +1,14 @@
 $('document').ready(function () {
-    console.log('Hi');
-})
+    let toggle = $('.combo-field-toggle');
+    let passwordField = $('.combo-field.password-field');
 
-$("#toggleButton").on("click", function() {
-    if ($("#password-input").attr('type') === 'password'){
-        $("#password-input").attr('type', 'text');
-        $("#toggleButton").html('<i class="fas fa-eye"></i>');
-
-    }else {
-        $("#password-input").attr('type', 'password');
-        $("#toggleButton").html('<i class="fas fa-eye-slash"></i>');
-    }
+    toggle.on('click', function () {
+        if (passwordField.attr('type') === 'password') {
+            passwordField.attr('type', 'text');
+            toggle.children().addClass('fa-eye').removeClass('fa-eye-slash');
+        } else {
+            passwordField.attr('type', 'password');
+            toggle.children().addClass('fa-eye-slash').removeClass('fa-eye');
+        }
+    })
 });
