164ffd2a8cf95992ca77e3ecdb6d8ec0ca55811b
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-11-08 16:24:49 +0700

    Added many to many relationship

--- app/models.py
+++ app/models.py
@@ -18,13 +18,29 @@ class Category(db.Model):
     def get_name(self):
         return self.name
 
+interested_events = db.Table('interested_events',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.event_id'))
+)
 
+attending_events = db.Table('attending_events',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.event_id'))
+)
+
+attended_events = db.Table('attended_events',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.user_id')),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.event_id'))
+)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    interested = db.relationship('Event', secondary=interested_events, backref=db.backref('interest',lazy='dynamic'))
+    attending = db.relationship('Event', secondary=attending_events, backref=db.backref('attending',lazy='dynamic'))
+    attended = db.relationship('Event', secondary=attended_events, backref=db.backref('attended',lazy='dynamic'))
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -35,8 +51,17 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def interested_count(self):
+        return len(self.interested)
+
+    def attending_count(self):
+        return len(self.attending)
+
+    def attended_count(self):
+        return len(self.attended)
+
 class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    event_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True)
     description = db.Column(db.String(250))
     location = db.Column(db.String(100), index=True)
