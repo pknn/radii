ef44990121a987e77b53a63d8f01c2a8f038e324
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-11-12 21:18:29 +0700

    Edit deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-scp -o "StrictHostKeyChecking no" docker-compose.yml pakanon_pk@$IP:~/
-ssh -o "StrictHostKeyChecking no" pakanon_pk@$IP "yes | sudo docker image prune && sudo docker-compose pull && sudo docker-compose up -d --force-recreate"
+scp -o "StrictHostKeyChecking no" docker-compose.yml pakanon_pk@$IP:~/radii-app
+ssh -o "StrictHostKeyChecking no" pakanon_pk@$IP "cd radii-app && yes | sudo docker image prune && sudo docker-compose pull && sudo docker-compose up -d --force-recreate"
