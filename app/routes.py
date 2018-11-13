7764daeb06e3c5ed3d51f73c7c720395c373e4ff
benzsupaluk <supaluk.j@ku.th>
2018-11-13 22:27:59 +0700

    Create path for user

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 import os
 from flask import render_template
 from app import app
+
 # from flask_login import current_user
 
 @app.route("/")
@@ -12,6 +13,7 @@ def index():
 def event():
     return render_template("event.html", title="Event")
 
-@app.route("/user")
-def user():
-    return render_template("user_profile.html", title="User")
+@app.route("/user/<username>")
+def user(username):
+    return render_template("user_profile.html", title="User", username=username)
+
