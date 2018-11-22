01f23a0f1056b5db4e968fe79e78471309dbdfe2
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-11-22 13:12:52 +0700

    Edit stylesheet

--- app/static/scripts/main.js
+++ app/static/scripts/main.js
@@ -1,6 +1,17 @@
 $('document').ready(function () {
-    console.log('Hi');
-})
+    let toggle = $('.combo-field-toggle');
+    let passwordField = $('.combo-field.password-field');
+
+    toggle.on('click', function () {
+        if (passwordField.attr('type') === 'password') {
+            passwordField.attr('type', 'text');
+            toggle.children().addClass('fa-eye').removeClass('fa-eye-slash');
+        } else {
+            passwordField.attr('type', 'password');
+            toggle.children().addClass('fa-eye-slash').removeClass('fa-eye');
+        }
+    })
+});
 
 let numberOfEvent = $('.grid .services').length;
 let limitEvent = 9;
