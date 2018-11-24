a4e7d110b221580c48254f786c91f6c8eeef46b6
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-11-25 01:02:31 +0700

    Edit test

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,12 +1,12 @@
 from app.models import User
 
-def test_event_password():
-    u = User(username='poom', email='poom@example.com')
-    u.set_password('mypassword')
 
-    assert not u.check_password('anotherpassword')
-    assert u.check_password('mypassword')
+def test_event_password():
+    u = User(display_name="poom", email="poom@example.com", password="mypassword")
+    assert not u.check_password("anotherpassword")
+    assert u.check_password("mypassword")
+
 
 def test_event_username():
-    u = User(username='poom', email='poom@example.com')
+    u = User(display_name="poom", email="poom@example.com")
     assert u
