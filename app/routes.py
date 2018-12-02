9df8aaa2e88dbe40731ad720381d9e8defc2154c
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-12-03 04:06:55 +0700

    Add cookie setting

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect, url_for, request, make_response
+from app import app, auth
 from app.models import User, Event
 from datetime import datetime, timedelta
+from flask_dance.contrib.github import github
 
 
 @app.route("/")
@@ -13,23 +14,34 @@ def index():
 def event():
     return render_template("event.html", title="Event")
 
+
 @app.route("/event/<event_id>")
 def event_descript(event_id):
     event_info = Event.query.filter_by(event_id=event_id).first()
     return render_template("event_description.html", event_info="event_info")
+
 
 @app.route("/login_github")
 def login_github():
     if not github.authorized:
         return redirect(url_for("github.login"))
     else:
-        resp = github.get("/user")
-        user_json = resp.json()
+        user_response = github.get("/user")
+        user_json = user_response.json()
         user = auth.oauth(user_json["login"], user_json["email"])
-        return jsonify(user.jsonify())
+        response = make_response(redirect(url_for("index")))
+        response.set_cookie("id", str(user.id))
+        response.set_cookie("email", user.email)
+        return response
 
 
 @app.route("/register", methods=["POST"])
 def register():
     display_name, email, password = request.form.values()
-    return jsonify(auth.register(display_name, email, password).jsonify())
+    auth.register(display_name, email, password)
+    return redirect(url_for("index"))
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    pass
