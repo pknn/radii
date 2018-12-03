6384324afae86f547e460224f8b33cf574bf7141
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-12-03 16:47:26 +0700

    Added event-user liked relationship

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ from flask_dance.consumer.backend.sqla import OAuthConsumerMixin
 from flask_login import UserMixin
 from app import db
 
-
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True)
     name = db.Column(db.String(64), index=True)
@@ -90,11 +89,18 @@ class Event(db.Model):
         return self.attended
 
 
+liked_user = ('liked_user',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
+)
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     display_name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    liked = db.relationship('Event', secondary=liked, lazy="subquery", backref=db.backref('users', lazy=True))
 
     def __init__(self, id=None, display_name="", email="", password=""):
         self.display_name = display_name
