46be63353db7edee45dd504b3477bc3feca90f93
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-12-03 17:07:53 +0700

    Edit secondary relationship

--- app/models.py
+++ app/models.py
@@ -100,7 +100,7 @@ class User(db.Model):
     display_name = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    liked = db.relationship('Event', secondary=liked, lazy="subquery", backref=db.backref('users', lazy=True))
+    liked = db.relationship('Event', secondary=liked_user, lazy="subquery", backref=db.backref('users', lazy=True))
 
     def __init__(self, id=None, display_name="", email="", password=""):
         self.display_name = display_name
