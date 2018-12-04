927dc548f827789ec1ee621d09c63501776fc374
Pakanon Pantisawat <pakanon.pk@gmail.com>
2018-12-04 09:59:39 +0700

    Move Authentication method to controller.py

--- app/controllers.py
+++ app/controllers.py
@@ -1,5 +1,6 @@
-from app import db
+from app import db, login_manager
 from app.models import User, Event, Category
+from flask_login import login_user, current_user
 
 
 class UserController:
@@ -26,3 +27,41 @@ class EventController:
         db.session.add(event)
         db.session.commit()
         return event
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+
+class AuthController:
+    @staticmethod
+    def oauth(display_name, email):
+        user = User.query.filter_by(display_name=display_name).first()
+        if user is None:
+            user = UserController.create_user(display_name, email)
+        login_user(user)
+        return user
+
+    @staticmethod
+    def register(display_name, email, password):
+        new_user = User(display_name=display_name, email=email, password=password)
+        UserController.create_user(new_user)
+        return new_user
+
+    @staticmethod
+    def login(email, password):
+        if current_user.is_authentucated:
+            # DO SOMETHING
+            pass
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if user.check_password(password):
+                # DO SOMETHING
+                pass
+            else:
+                # DO SOMETHING
+                pass
+        else:
+            # DO SOMETHING
+            pass
