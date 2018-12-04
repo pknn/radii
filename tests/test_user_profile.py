f91bed3d8516d7c2f860cd94664c34af3edc880c
Thanapoom Rattanathumawat <thanapoom.ta@ku.th>
2018-12-04 23:47:22 +0700

    Added more tests

--- tests/test_user_profile.py
+++ tests/test_user_profile.py
@@ -30,6 +30,22 @@ class TestUserRevise(unittest.TestCase):
         u.like(e)
         self.assertTrue(u.unlike(444))
 
+    def test_liked_events(self):
+        e = Event(
+            "test",
+            "description",
+            "location",
+            "url",
+            date_time=datetime.datetime.utcnow()
+        )
+        e.id = 678
+        u = User(display_name='Zak')
+
+        u.like(e)
+        self.assertTrue(len(u.liked_events),1)
+        u.unlike(e)
+        self.assertTrue(len(u.liked_events),0)
+
     def test_attending(self):
         e = Event(
             "test",
@@ -55,3 +71,19 @@ class TestUserRevise(unittest.TestCase):
         u = User(display_name='Dave')
         u.attending(e)
         self.assertTrue(u.unattending(145))
+
+    def test_attended(self):
+        e = Event(
+            "test",
+            "description",
+            "location",
+            "url",
+            date_time=datetime.datetime.utcnow()
+        )
+        e.id = 555
+        u = User(display_name='Jay')
+
+        u.attending(e)
+        self.assertEqual(len(u.attending_events),1)
+        u.unattending(e)
+        self.assertTrue(len(u.attending_events),0)
