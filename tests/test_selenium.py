c7df84a3c7a658616a185bbe4a02d66f0a53974c
benzsupaluk <supaluk.j@ku.th>
2018-12-06 01:20:46 +0700

    Update selenium tests

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,41 +1,36 @@
 import unittest
-from selenium import webdriver
+import requests
 
+from selenium import webdriver
 
 class TestSelenium(unittest.TestCase):  
   def setUp(self):
     options = webdriver.ChromeOptions()
+    options.add_argument('--start-maximized')
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
+
     self.browser = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
 
 
-  def test_event_invalid_url(self):
+  def test_search_homepage(self):
     browser = self.browser
-    browser.get('https://radii.devinpeace.com/event')
-    links = []
-    tag_name = browser.find_elements_by_tag_name('a')
-    for t in tag_name:
-      links.append(t.get_attribute("href"))    
-    invalid_links = []
-    for l in links:    
-      resp = requests.head(str(l))
-      if resp.status_code != 200:
-        invalid_links.append(l)    
-    self.assertEqual(0, len(invalid_links))
+    browser.get('https://radii.devinpeace.com')
+    self.assertIn("Radii - Find your radius", browser.title)
+    search = browser.find_element_by_id('search-input')
+    search.send_keys("party")
+    search_button = browser.find_element_by_id('search-button')
+    search_button.click()
+    browser.close()
 
   def test_login(self):
     browser = self.browser
     browser.get('https://radii.devinpeace.com')
-    login_modal = browser.find_element_by_class_name('nav-link')
-    login_modal.click()
     email = browser.find_element_by_name('email')
     password = browser.find_element_by_name('password')
-    email[0].send_keys("test@example.com")
-    password[0].send_keys("test1234")
-    login_button = browser.find_element_by_id('loginbutton')
+    login_button = browser.find_element_by_xpath("//button[@type='submit']")
     login_button.click()
-    
-  def tearDown(self):
-    self.browser.close()
+    browser.close()
   
   
 
